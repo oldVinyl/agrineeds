583aee45077b86f04a969b9b2f7ca6c50ff2c505
oldVinyl <neamewu@gmail.com>
2025-08-18 17:25:04 +0000

    test(money): align tests to GH₵ symbol output from formatGHS

--- tests/money.test.js
+++ tests/money.test.js
@@ -2,7 +2,7 @@
 import { formatGHS } from "../js/utils/money.js";
 
 export async function suiteMoney(t) {
-  t.equal("formatGHS basic", formatGHS(120000), "GHS 1,200.00");
-  t.equal("formatGHS zero", formatGHS(0), "GHS 0.00");
-  t.equal("formatGHS cents rounding", formatGHS(85), "GHS 0.85");
+  t.equal("formatGHS basic", formatGHS(120000), "GH₵1,200.00");
+  t.equal("formatGHS zero", formatGHS(0), "GH₵0.00");
+  t.equal("formatGHS cents rounding", formatGHS(85), "GH₵0.85");
 }
